8e91530f367105ca9135618a685bf6c36a6c781a
kalleasgard <kalleasgard@gmail.com>
2015-01-01 18:11:30 +0100

    Changed to My Contact

--- MyTestGitMvcApplication/Controllers/HomeController.cs
+++ MyTestGitMvcApplication/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace MyTestGitMvcApplication.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "My contact page.";
 
             return View();
         }
